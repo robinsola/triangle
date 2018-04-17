8960d424cf5481ec0531848ddde2f06e7ebdc716
Joe McKinney and Robin Sola <robin@>
2018-04-17 10:03:19 -0700

    add text to src/main.js src/styles.css src/triangleLogic.js git status

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import { Triangle } from './triangleLogic';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+
+
+
+$(document).ready(function() {
+  $("#sideLengths").submit(function(event) {
+    event.preventDefault();
+    var side1 = parseInt($("#side1").val());
+    var side2 = parseInt($("#side2").val());
+    var side3 = parseInt($("#side3").val());
+    var newTriangle = new Triangle(side1, side2, side3);
+    var answer = newTriangle.triangleChecker();
+    $(".outputInput").text(answer);
+    $(".output").show();
+  });
+});
