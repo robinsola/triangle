e93696f702ff25a340482f018e8b7f4e21e892a5
Joe McKinney and Robin Sola <joe@>
2018-04-17 13:33:21 -0700

    add jasmine and karma to practice test runs and debugging.

--- src/triangleLogic.js
+++ src/triangleLogic.js
@@ -1,22 +1,30 @@
-var type = "";
-
+// var type = "";
+//
 export function Triangle(side1, side2, side3) {
   this.side1 = side1;
   this.side2 = side2;
   this.side3 = side3;
 }
 
-Triangle.prototype.triangleChecker = function(side1, side2, side3) {
-  if (isNaN(this.side1) || isNaN(this.side2) || isNaN(this.side3)) {
-    type = "please enter numbers";
-  } else if ((this.side1 + this.side2 <= this.side3) || (this.side2 + this.side3 <= this.side1) || (this.side1 + this.side3 <= this.side2)) {
-    type = "This isn't a triangle";
-  } else if (this.side1 === this.side2 && this.side2 === this.side3) {
-    type = "equalateral";
-  } else if ((this.side1 === this.side2 && this.side2 !== this.side3) || (this.side1 !== this.side2 && this.side2 === this.side3) || (this.side1 === this.side3 && this.side2 !== this.side3)) {
-    type = "isoceles";
-  } else if (this.side1 !== this.side2 && this.side1 !== this.side3) {
-    type = "scalene";
-  }
-  return type;
+Triangle.prototype.checkType = function() {
+  debugger;
+   if ((this.side1 < (this.side2 + this.side3)) || (this.side2 > (this.side1 + this.side3)) || (this.side3 > (this.side1 + this.side2))) {
+     return "not a triangle";
+   }
 };
+
+//
+// Triangle.prototype.triangleChecker = function() {
+//   if (isNaN(this.side1) || isNaN(this.side2) || isNaN(this.side3)) {
+//     type = "please enter numbers";
+//   } else if ((this.side1 + this.side2 <= this.side3) || (this.side2 + this.side3 <= this.side1) || (this.side1 + this.side3 <= this.side2)) {
+//     type = "This isn't a triangle";
+//   } else if (this.side1 === this.side2 && this.side2 === this.side3) {
+//     type = "equalateral";
+//   } else if ((this.side1 === this.side2 && this.side2 !== this.side3) || (this.side1 !== this.side2 && this.side2 === this.side3) || (this.side1 === this.side3 && this.side2 !== this.side3)) {
+//     type = "isoceles";
+//   } else if (this.side1 !== this.side2 && this.side1 !== this.side3) {
+//     type = "scalene";
+//   }
+//   return type;
+// };
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,14 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/main.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  devtool: 'eval-src-map',
+  devtool: 'eval-source-map',
   devServer: {
     contentBase: './dist'
   },
@@ -25,15 +24,18 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\css$/,
+        test: /\.css$/,
         use: [
-          'style-loader'
+          'style-loader',
           'css-loader'
         ]
-      }
+      },
       {
         test: /\.js$/,
-        exclude: /node_modules/,
+        exclude: [
+          /node_modules/,
+          /spec/
+        ],
         loader: "eslint-loader"
       }
     ]
