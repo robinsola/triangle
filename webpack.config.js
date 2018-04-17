c3538fe7d3b382ec7335ff3e0280e47b889f4cce
Joe McKinney and Robin Sola <robin@>
2018-04-17 09:48:08 -0700

    add more dependecies to our package.json and will modify webpack.config.js and will create an .eslintrc file

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   entry: './src/main.js',
@@ -8,7 +9,12 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  devtool: 'eval-src-map',
+  devServer: {
+    contentBase: './dist'
+  },
   plugins: [
+    new UglifyJsPlugin({ sourceMap: true }),
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: 'triangle',
@@ -24,6 +30,11 @@ module.exports = {
           'style-loader'
           'css-loader'
         ]
+      }
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader"
       }
     ]
   }
